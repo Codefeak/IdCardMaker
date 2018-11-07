240f1a861e11296dfd70c776523b8cfc1cf9aec3
codefeak <codefever17@gmail.com>
2018-11-07 11:24:09 +0200

    fixed boilerplate

--- src/index.js
+++ src/index.js
@@ -1,10 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import { HashRouter as Router } from 'react-router-dom';
+
 import './index.css';
+
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import reducers from './store/reducers';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+/*eslint-disable */
+const composeEnhancers =
+  process.env.NODE_ENV === 'development' &&
+  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+    : compose;
+/* eslint-enable */
+const store = createStore(reducers);
+ReactDOM.render(
+  <Provider store={store}>
+    <Router>
+    <App />
+    </Router>
+  </Provider>,
+  document.getElementById('root'),
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
