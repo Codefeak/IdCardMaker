1788a5dce149119a5d0039e8e5a4034c997cf721
codefeak <codefever17@gmail.com>
2018-11-16 16:26:48 +0200

    Updates

---
+++ precache-manifest.f61f7f81d8b83a56d877761d3ced4099.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "ed2599dfab7ad0959ee9",
+    "url": "/IdCardMaker/static/css/main.3feeac0a.chunk.css"
+  },
+  {
+    "revision": "ed2599dfab7ad0959ee9",
+    "url": "/IdCardMaker/static/js/main.ed2599df.chunk.js"
+  },
+  {
+    "revision": "00418d0eedb87ea44df2",
+    "url": "/IdCardMaker/static/js/1.00418d0e.chunk.js"
+  },
+  {
+    "revision": "a18630004aebd4a454fc",
+    "url": "/IdCardMaker/static/js/runtime~main.a1863000.js"
+  },
+  {
+    "revision": "5add75025c6642d96f9568a5de9e4d6b",
+    "url": "/IdCardMaker/static/media/ID_bg_chip.5add7502.jpg"
+  },
+  {
+    "revision": "c154ab0fb52b9ad8435c70630336cef2",
+    "url": "/IdCardMaker/static/media/ID_back_chip.c154ab0f.jpg"
+  },
+  {
+    "revision": "afb58b4186a4c7a9802e72b4f33a97b3",
+    "url": "/IdCardMaker/index.html"
+  }
+];
